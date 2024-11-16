a2c0a14122908e2b88038b843266ed10b1b75f75
Imtiaz <imtiaz@bdjobs.local>
2024-11-16 12:46:24 +0600

    working

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { Child1Component } from "../child-1/child-1.component";
+import { Child2Component } from "../child-2/child-2.component";
 
 @Component({
   selector: 'app-parent',
   standalone: true,
-  imports: [],
+  imports: [Child1Component, Child2Component],
   templateUrl: './parent.component.html',
   styleUrl: './parent.component.css'
 })
